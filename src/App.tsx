9661a1ace0594ca95a38997635b0419498467ec8
Mahmudul Hasan Nayeem <hasan@basikmarketing.in>
2024-12-12 13:54:56 +0600

    feat: added unit converter

--- src/App.tsx
+++ src/App.tsx
@@ -2,61 +2,61 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/Home/Home";
+import ColorPicker from "./pages/Home/Tools/ColorPicker/ColorPicker";
+import FontPicker from "./pages/Home/Tools/FontPicker/FontPicker";
+import ImageEditor from "./pages/Home/Tools/ImageEditor/ImageEditor";
+import ImageToBase64 from "./pages/Home/Tools/ImageToBase64/ImageToBase64";
 import JSONFormatter from "./pages/Home/Tools/JsonFormatter/JsonFormatter";
 import MarkdownToLinkedIn from "./pages/Home/Tools/MarkdownToLinkedIn/MarkdownToLinkedIn";
-import FontPicker from "./pages/Home/Tools/FontPicker/FontPicker";
-import ImageToBase64 from "./pages/Home/Tools/ImageToBase64/ImageToBase64";
-import URLEncoder from "./pages/Home/Tools/URLEncoder/URLEncoder";
 import StringConverters from "./pages/Home/Tools/StringConverters/StringConverters";
-import ColorPicker from "./pages/Home/Tools/ColorPicker/ColorPicker";
-import ImageEditor from "./pages/Home/Tools/ImageEditor/ImageEditor";
+import URLEncoder from "./pages/Home/Tools/URLEncoder/URLEncoder";
+import UnitConverter from "./pages/Home/Tools/UnitConverter/UnitConverter";
 const fonts = [
-  'Arial, sans-serif',
-  'Verdana, Geneva, sans-serif',
-  'Tahoma, Geneva, sans-serif',
-  'Trebuchet MS, Helvetica, sans-serif',
-  'Times New Roman, Times, serif',
-  'Georgia, serif',
-  'Garamond, serif',
-  'Courier New, Courier, monospace',
-  'Brush Script MT, cursive',
-  'Impact, Charcoal, sans-serif',
-  'Comic Sans MS, cursive, sans-serif',
-  'Lucida Console, Monaco, monospace',
-  'Arial Black, Gadget, sans-serif',
-  'Lucida Sans Unicode, Lucida Grande, sans-serif',
-  'Palatino Linotype, Book Antiqua, Palatino, serif',
-  'Helvetica, Arial, sans-serif',
-  'Gill Sans, Geneva, sans-serif',
-  'Century Gothic, sans-serif',
-  'Copperplate, Papyrus, fantasy',
-  'Papyrus, fantasy',
-  'Bookman, serif',
-  'Candara, sans-serif',
-  'Franklin Gothic Medium, Arial Narrow, Arial, sans-serif',
-  'Consolas, monaco, monospace',
-  'Baskerville, serif',
-  'Futura, sans-serif',
-  'Geneva, Tahoma, Verdana, sans-serif',
-  'Hoefler Text, serif',
-  'Optima, sans-serif',
-  'SignPainter, cursive',
-  'Andale Mono, monospace',
-  'Charter, serif',
-  'Big Caslon, serif',
-  'Calibri, sans-serif',
-  'Cambria, serif',
-  'Didot, serif',
-  'Lucida Bright, Georgia, serif',
-  'Goudy Old Style, serif',
-  'Rockwell, serif'
+  "Arial, sans-serif",
+  "Verdana, Geneva, sans-serif",
+  "Tahoma, Geneva, sans-serif",
+  "Trebuchet MS, Helvetica, sans-serif",
+  "Times New Roman, Times, serif",
+  "Georgia, serif",
+  "Garamond, serif",
+  "Courier New, Courier, monospace",
+  "Brush Script MT, cursive",
+  "Impact, Charcoal, sans-serif",
+  "Comic Sans MS, cursive, sans-serif",
+  "Lucida Console, Monaco, monospace",
+  "Arial Black, Gadget, sans-serif",
+  "Lucida Sans Unicode, Lucida Grande, sans-serif",
+  "Palatino Linotype, Book Antiqua, Palatino, serif",
+  "Helvetica, Arial, sans-serif",
+  "Gill Sans, Geneva, sans-serif",
+  "Century Gothic, sans-serif",
+  "Copperplate, Papyrus, fantasy",
+  "Papyrus, fantasy",
+  "Bookman, serif",
+  "Candara, sans-serif",
+  "Franklin Gothic Medium, Arial Narrow, Arial, sans-serif",
+  "Consolas, monaco, monospace",
+  "Baskerville, serif",
+  "Futura, sans-serif",
+  "Geneva, Tahoma, Verdana, sans-serif",
+  "Hoefler Text, serif",
+  "Optima, sans-serif",
+  "SignPainter, cursive",
+  "Andale Mono, monospace",
+  "Charter, serif",
+  "Big Caslon, serif",
+  "Calibri, sans-serif",
+  "Cambria, serif",
+  "Didot, serif",
+  "Lucida Bright, Georgia, serif",
+  "Goudy Old Style, serif",
+  "Rockwell, serif",
 ];
 
 function App() {
   return (
     <>
       <Routes>
-
         {/* <Route
           path="/"
           element={authUser ? <Home /> : <Navigate to={"/login"} />}
@@ -71,6 +71,7 @@ function App() {
         <Route path="/json-formatter" element={<JSONFormatter />} />
         <Route path="/url-encoder" element={<URLEncoder />} />
         <Route path="/string-converters" element={<StringConverters />} />
+        <Route path="/unit-converters" element={<UnitConverter />} />
       </Routes>
     </>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,24 +8,37 @@ import {
   FaLink,
   FaMarkdown,
   FaPalette,
-
 } from "react-icons/fa";
+import { Si500Px } from "react-icons/si";
 import { Link } from "react-router-dom";
-import Footer from "../../componets/Footer/Footer";
-import Navbar from "../../componets/Navbar/Navbar";
 import Contact from "../../componets/Contact/Contact";
 import Contributors from "../../componets/Contributors/Contributors";
+import Footer from "../../componets/Footer/Footer";
+import Navbar from "../../componets/Navbar/Navbar";
 
 const HomePage = () => {
   const tools = [
-    { name: "Markdown to LinkedIn", path: "/markdown-to-linkedin", icon: <FaMarkdown /> },
+    {
+      name: "Markdown to LinkedIn",
+      path: "/markdown-to-linkedin",
+      icon: <FaMarkdown />,
+    },
     { name: "Image Editor", path: "/image-editor", icon: <FaEdit /> },
     { name: "Color Picker", path: "/color-picker", icon: <FaPalette /> },
     { name: "Font Picker", path: "/font-picker", icon: <FaFont /> },
-    { name: "Image to Base64 And Decode", path: "/image-to-base64", icon: <FaImage /> },
+    {
+      name: "Image to Base64 And Decode",
+      path: "/image-to-base64",
+      icon: <FaImage />,
+    },
     { name: "JSON Formatter", path: "/json-formatter", icon: <FaCode /> },
     { name: "URL Encoder and Decoder", path: "/url-encoder", icon: <FaLink /> },
-    { name: "String Converters", path: "/string-converters", icon: <FaExchangeAlt /> },
+    {
+      name: "String Converters",
+      path: "/string-converters",
+      icon: <FaExchangeAlt />,
+    },
+    { name: "Unit Converters", path: "/unit-converters", icon: <Si500Px /> },
 
     // Add more tools here with appropriate icons
   ];
@@ -41,7 +54,9 @@ const HomePage = () => {
           </p>
         </header>
         <section className="mb-16">
-          <h2 className="text-3xl font-semibold mb-8 text-gray-800">Available Tools</h2>
+          <h2 className="text-3xl font-semibold mb-8 text-gray-800">
+            Available Tools
+          </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {tools.map((tool) => (
               <Link
@@ -49,8 +64,12 @@ const HomePage = () => {
                 key={tool.name}
                 className="card bg-white shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 ease-in-out p-8 text-center rounded-lg "
               >
-                <div className="text-6xl mb-6 text-blue-500 flex justify-center ">{tool.icon}</div>
-                <h3 className="text-2xl font-semibold text-gray-900">{tool.name}</h3>
+                <div className="text-6xl mb-6 text-blue-500 flex justify-center ">
+                  {tool.icon}
+                </div>
+                <h3 className="text-2xl font-semibold text-gray-900">
+                  {tool.name}
+                </h3>
               </Link>
             ))}
           </div>
